8a080361457b4a7542b29f74aa5ec980e668c2c8
mSharifHub <mosharifemail@gmail.com>
2023-08-04 21:34:56 -0700

    files organization

---
+++ src/Components/BookComponent.js
@@ -0,0 +1,7 @@
+const Book = () => {
+    return <article></article>;
+};
+
+const Image = () => <h2>Image Place Holder</h2>;
+const Title = () => <h2>Book Title Place Holder</h2>;
+const Author = () => <h2>Author Name Place Holder</h2>;
