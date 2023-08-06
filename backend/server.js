0cebcd25994aa891e3baacb2b3fb6c42aeab23e9
mSharifHub <mosharifemail@gmail.com>
2023-08-06 03:08:00 -0700

    back end api fetching and responding

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const API_KEY = process.env.GOOGLE_BOOKS_API_KEY;
+
+app.use(cors());
+
+/*
+    End point that receives the query request from client side
+
+*/
+app.get("/book-list", async (req, res) => {
+    // deconstruction instead of using req.query.query ( first query is the query and the second is the values)
+    const { query } = req.query;
+
+
+    if (!query) {
+        return res.status(400).json({ error: `Query parameter is required` });
+    }
+
+    try {
+        // Sending a GET request to the google API using the query and the API_KEY for the application
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`);
+
+        // Extracting the data as a new array to send to the front end book component
+        const books = response.data.items.map((item) => ({
+            title: item.volumeInfo.title,
+            image: item.volumeInfo.imageLinks?.thumbnail,
+            alt: item.volumeInfo.title,
+            author: item.volumeInfo.authors?.join(", "),
+        }));
+
+        // setting as JSON format and send JSON string as the response body
+        res.json(books);
+    } catch (err) {
+        res.status(500).json({ error: `Failed to fetch books` });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://local:host:${PORT}`);
+});
--- src/Components/BookListComponent.js
+++ src/Components/BookListComponent.js
@@ -2,39 +2,53 @@ import React, { useState } from "react";
 
 import Book from "./BookComponent";
 
-const Books = [
-    {
-        title: "Learning React: Modern Patterns for Developing React Apps 2nd Edition",
-        image: "./images/bookReact.jpg",
-        alt: "Learning React: Modern Patterns for Developing React Apps 2nd Edition",
-        author: "Alex Banks",
-    },
-
-    {
-        title: "Node.js: The Comprehensive Guide to Server-Side JavaScript Programming",
-        image: "https://m.media-amazon.com/images/P/B0B5B34PYT.01._SCLZZZZZZZ_SX500_.jpg",
-        alt: "Node.js: The Comprehensive Guide to Server-Side JavaScript Programming",
-        author: "Sebastian Springer",
-    },
-];
-
 const BookList = () => {
     const [searchTerm, setSearchTerm] = useState("");
+    const [books, setBooks] = useState([]);
 
-    const filteredBooks = Books.filter((book) => book.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    const handleSearchChange = async (event) => {
+        const query = event.target.value;
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
+        //set the query value to send to the backend for google API
+        setSearchTerm(query);
+
+        if (query) {
+            try {
+                const response = await fetch(`http://localhost:3000/book-list?query=${query}&maxResults=40`);
+
+                if (response.ok) {
+                    const fetchedBooks = await response.json();
+                    //Update the state to the data sent from back end
+                    setBooks(fetchedBooks);
+                } else {
+                    console.error("Failed to fetch books");
+                    setBooks([]);
+                }
+            } catch (err) {
+                console.error(err);
+                setBooks([]);
+            }
+        } else {
+            setBooks([]);
+        }
     };
 
     return (
+        /*
+            Passing the props down to SearchForm component
+            searchTerm is what the user is inputing
+            handleSearchChange is the action to the user input
+
+            Books will be the data extracted from google API after
+            the response received as status 200 
+        */
         <>
             <SearchForm searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
 
             <section className="booklist">
-                {filteredBooks.map((book, index) => {
-                    return <Book {...book} key={index} />;
-                })}
+                {books.map((book, index) => (
+                    <Book {...book} key={index} />
+                ))}
             </section>
         </>
     );
